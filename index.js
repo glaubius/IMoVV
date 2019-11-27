f387b6dac64780778594e2c3fcc50c751466b0fd
Jen Glaubius <glaubius@gmail.com>
2019-11-27 14:37:10 -0500

    add tile server, center, and zoom

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+window.onload = function () {
+  var basemap = new L.StamenTileLayer("terrain");
+  var map = new L.map("my-map", {
+    center: new L.LatLng(38.1500, 22.0833),
+    zoom: 10
+  });
+  map.addLayer(basemap);
+};
