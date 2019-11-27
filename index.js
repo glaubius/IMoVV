dd42489e7954d638f6ca641716bf75b203b24d74
Jen Glaubius <glaubius@gmail.com>
2019-11-27 16:26:34 -0500

    change zoom and add navbar

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 window.onload = function () {
   var basemap = new L.StamenTileLayer("terrain");
-  var map = new L.map("my-map", {
-    center: new L.LatLng(38.1500, 22.0833),
-    zoom: 10
-  });
+  var map = new L.map("my-map", {scrollWheelZoom:false}).setView([38.1500, 22.0833],10);
+    
   map.addLayer(basemap);
 };
